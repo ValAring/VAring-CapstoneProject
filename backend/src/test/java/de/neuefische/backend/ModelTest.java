4347be0a03b18895070108bdd532824b9b7cd941
valeriearing <the.v.aring@gmail.com>
2023-10-25 11:10:09 +0200

    Added changed Model Tests

--- backend/src/test/java/de/neuefische/backend/ModelTest.java
+++ backend/src/test/java/de/neuefische/backend/ModelTest.java
@@ -4,6 +4,7 @@ import de.neuefische.backend.model.Project;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ModelTest {
     private static Project createProject() {
@@ -11,37 +12,37 @@ class ModelTest {
     }
 
     @Test void testWithId            () { assertEquals(new Project(" ", "a", "t", "yesterday", "today"), createProject().withId          (" ")); }
-    @Test void testWhenAuthorChange_IDEqual() {
+    @Test void testWhenIdChange_IDNotEqual() {
         Project originalProject = new Project("i", "a", "t", "yesterday", "today");
-        Project newProject = originalProject.withAuthor("new");
-        assertEquals(originalProject.id(), newProject.id());
+        Project newProject = originalProject.withId("new");
+        assertNotEquals(originalProject.id(), newProject.id());
     }
 
     @Test void testWithAuthor        () { assertEquals(new Project("i", " ", "t", "yesterday", "today"), createProject().withAuthor     (" ")); }
-    @Test void testWhenDescChange_AuthorEqual() {
+    @Test void testWhenAuthorChange_AuthorNotEqual() {
         Project originalProject = new Project("i", "a", "t", "yesterday", "today");
-        Project newProject = originalProject.withDescription("new");
-        assertEquals(originalProject.author(), newProject.author());
+        Project newProject = originalProject.withAuthor("new");
+        assertNotEquals(originalProject.author(), newProject.author());
     }
 
     @Test void testWithDescription   () { assertEquals(new Project("i", "a", " ", "yesterday", "today"), createProject().withDescription(" ")); }
-    @Test void testWhenAuthorChange_DescrEqual() {
+    @Test void testWhenDescrChange_DescrNotEqual() {
         Project originalProject = new Project("i", "a", "t", "yesterday", "today");
-        Project newProject = originalProject.withAuthor("new");
-        assertEquals(originalProject.description(), newProject.description());
+        Project newProject = originalProject.withDescription("new");
+        assertNotEquals(originalProject.description(), newProject.description());
     }
 
     @Test void testWithTimeCreated   () { assertEquals(new Project("i", "a", "t", " ", "today"), createProject().withTimeCreated  (" ")); }
-    @Test void testWhenAuthorChange_TimeCreatedEqual() {
+    @Test void testWhenTimeCreatedChange_TimeCreatedNotEqual() {
         Project originalProject = new Project("i", "a", "t", "yesterday", "today");
-        Project newProject = originalProject.withAuthor("new");
-        assertEquals(originalProject.timeCreated(), newProject.timeCreated());
+        Project newProject = originalProject.withTimeCreated("new");
+        assertNotEquals(originalProject.timeCreated(), newProject.timeCreated());
     }
 
     @Test void testWithLastEdited    () { assertEquals(new Project("i", "a", "t", "yesterday", " "), createProject().withLastEdited  (" ")); }
-    @Test void testWhenAuthorChange_lastEditedEqual() {
+    @Test void testWhenLastEditedChange_lastEditedEqual() {
         Project originalProject = new Project("i", "a", "t", "yesterday", "today");
-        Project newProject = originalProject.withAuthor("new");
-        assertEquals(originalProject.lastEdited(), newProject.lastEdited());
+        Project newProject = originalProject.withLastEdited("new");
+        assertNotEquals(originalProject.lastEdited(), newProject.lastEdited());
     }
 }
